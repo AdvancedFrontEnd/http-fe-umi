8ef4a0d5641e1c77d251616755631ababf51222d
anonyzhang <wydfzhang@163.com>
2023-08-25 00:00:33 +0800

    feat: 人员管理

--- src/pages/Project/ProjectConfig/index.tsx
+++ src/pages/Project/ProjectConfig/index.tsx
@@ -1,9 +1,55 @@
+import { Table, Tag } from 'antd';
+import styles from './index.css';
+
 const ProjectConfig = () => {
-    return (
-        <>
-        ProjectConfig
-        </>
-    )
-}
+  const dataSource = [
+    {
+      key: '1',
+      username: 'fy',
+      permission: '项目经理',
+      modify: '3天前',
+    },
+    {
+      key: '2',
+      username: 'ht',
+      permission: '平台用户',
+      modify: '1天前',
+    },
+    {
+      key: '3',
+      username: 'yzh',
+      permission: '平台用户',
+      modify: '2天前',
+    },
+  ];
+
+  const columns = [
+    {
+      title: '用户名',
+      dataIndex: 'username',
+      key: 'username',
+    },
+    {
+      title: '团队权限',
+      dataIndex: 'permission',
+      key: 'permission',
+      render: (_, { permission }) => (
+        <Tag color="green" key={permission}>
+          {permission}
+        </Tag>
+      ),
+    },
+    {
+      title: '最近修改',
+      dataIndex: 'modify',
+      key: 'modify',
+    },
+  ];
+  return (
+    <div className={styles.container}>
+      <Table dataSource={dataSource} columns={columns} />
+    </div>
+  );
+};
 
 export default ProjectConfig;
